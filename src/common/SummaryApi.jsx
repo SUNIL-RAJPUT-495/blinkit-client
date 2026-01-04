7b88b14d002357387f87b1a51eaa8d1816a54481
SUNIL-RAJPUT-495 <sunilsinghshekhawat495@gmail.com>
2026-01-04 15:22:33 +0530

    alll ok

--- src/common/SummaryApi.jsx
+++ src/common/SummaryApi.jsx
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://blinkit-server-sigma.vercel.app";
 
 const SummaryApi = {
   /* ================= USER ================= */
